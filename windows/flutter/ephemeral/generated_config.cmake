edf71c0a0b6ae2a42b01b022eeb1a7ba41386894
mc05527 <60295266+mc05527@users.noreply.github.com>
2023-03-23 12:08:02 +0500

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\HP\\Downloads\\flutter_windows_2.8.1-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\HP\\OneDrive\\Documents\\flutter_demo" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\HP\\Downloads\\flutter_windows_2.8.1-stable\\flutter"
+  "PROJECT_DIR=C:\\Users\\HP\\OneDrive\\Documents\\flutter_demo"
+  "FLUTTER_ROOT=C:\\Users\\HP\\Downloads\\flutter_windows_2.8.1-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\HP\\OneDrive\\Documents\\flutter_demo\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\HP\\OneDrive\\Documents\\flutter_demo"
+  "FLUTTER_TARGET=C:\\Users\\HP\\OneDrive\\Documents\\flutter_demo\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\HP\\OneDrive\\Documents\\flutter_demo\\.dart_tool\\package_config.json"
+)
